3ffb3014520b4b15f7c22e1cf7de8b12646dd7e6
meenasenthl1614 <103577640+meenasenthl1614@users.noreply.github.com>
2022-06-08 13:30:08 +0530

    Add files via upload

---
+++ factorial number.py
@@ -0,0 +1,36 @@
+# write a program to print factorial of a given number
+
+
+
+num = int(input("Enter a number:"))
+fact = 1
+for i in range(1, num + 1):
+    fact = fact * i
+print("factorial of num is:",fact)
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+num = int(input("Enter a number:"))
+fact = 1
+for i in range(1, num + 1):
+    fact = fact * i
+print("factorial of num is:",fact)
+
+ 
+
+
