44d3e2bbdff0bed740a685e903048063565032ec
meenasenthl1614 <103577640+meenasenthl1614@users.noreply.github.com>
2022-06-10 11:42:16 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,8 +1,3 @@
-from mmap import MADV_RANDOM
-
-
-MADV_RANDOM#palindrome
-
 s = input("enter the value:")
 
 reverse = s[::-1 ]
@@ -12,22 +7,3 @@ if(s == reverse):
 else:
     print("its a not palindrome number")
 
-
-
-
-
-def palindrome(no):
-    new = 0
-    while (no != 0):
-        new = new + (no % 10)
-        new = new * 10
-        no = no // 10
-
-    new = new / 10
-    return int(new)
-
-n=int(input('Enter the number'))
-if n == palindrome(n):
-    print(n , "is a palindrome")   
-else:
-    print(n , "is not a palindrome")  
